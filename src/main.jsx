92eb1637e8c0e23db67ef7fabd2131ab1f55dae5
Krishibot-ai <vivekn492@gmail.com>
2026-03-14 11:38:42 +0530

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import KrishiBot from './KrishiBot.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <KrishiBot />
-  </React.StrictMode>
-)
